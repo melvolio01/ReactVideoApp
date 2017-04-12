8aa84b9ed660f735e39dccb09ad7be1edd36d1f6
andy hunt <andyhunt200@hotmail.com>
2017-04-12 17:39:04 +0100

    First Commit

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,33 @@
 // React import statements needed for all components using JSX
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+//Creating 'class' allows component to communicate with other components - we are extending React component so that our class has its functionality
 
-const SearchBar = () => {
-  return <input />
-}
+//Functional components DO NOT HAVE STATE, only class-based ones do
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    //defining the property 'search term' on state
+    this.state = { term: ''};
+  }
+
+  //this.setState allows us to update the state (which was initially set up as above) - this.setState allows us to inform React of a change
+  render() {
+    return ( 
+      <div className="search-bar">
+        <input 
+          //initial value = empty string (term), this section makes the component a controlled component
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+      </div>  
+      );
+    }
+
+    onInputChange(term) {
+      this.setState({term});
+      this.props.onSearchTermChange(term);
+    }
+  }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-//unlike libraries (which are namespaced) we need to import our own files with a file path
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAXP4htP7AJBlLpDUd4FnefgG5iDGJGPno'
 
-// Create a new component, which should produce some HTML
 
-//Fat arrow is basically same as 'function'
-const App = () => {
-  //JSX allows us to use what looks like HTML as JavaScript
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+// Create a new component, which should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+      this.state = { 
+        videos: [], 
+        selectedVideo: null
+      };
+
+     this.videoSearch("Journey Don't Stop Believing");
+    }
+  
+  videoSearch(term){
+     YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+      });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
